36710d012590bad32d4b495378ce420c85439ed3
Brett Andrews <brettandrews3@gmail.com>
2021-07-07 17:06:18 -0500

    Refactored the initial variable/input/print method. Went from 24 rigid lines of code to 12 flexible ones.

--- refactoring_practice.py
+++ refactoring_practice.py
@@ -1,7 +1,7 @@
 # Refactoring practice. Initial idea came from Head First Learn to Code, p195.
 
 # Idea: Setting default values for users as we generate their avatars:
-
+"""
 hair_color = input('What color is your hair [brown]? ')
 if hair_color == '':
     hair_color = 'brown'
@@ -31,3 +31,22 @@ has_beard = input('Do they have a beard [no]? ')
 if has_beard == '':
     has_beard = 'no'
 print('They have', has_beard, 'beard.')
+"""
+
+"""Reworking the code: Refactoring"""
+# Here's where we're going to start using the refactoring process to simplify this load of code.
+# This get_attribute() prints the same data in 12 lines vs. 24 lines above. It's also more flexible.
+def get_attribute(query, default):
+    question = query + ' [' + default + ']? '
+    answer = input(question)
+    if answer == '':
+        answer = default
+    print(f'You chose {answer}.')
+    return answer
+
+hair_color = get_attribute('What hair color', 'brown')
+hair_length = get_attribute('What hair length', 'short')
+eye_color = get_attribute('What eye color', 'hazel')
+gender = get_attribute('What gender', 'nonbinary')
+glasses = get_attribute('Do they wear glasses', 'no')
+beard = get_attribute('Do they have a beard', 'no')
